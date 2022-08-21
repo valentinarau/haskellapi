81c30e9c73bf85650db485fb1ae4bf8bb7db3439
valentinarau <vraurobayo@frba.utn.edu.ar>
2022-08-21 10:21:31 -0300

    template

---
+++ app/Main.hs
@@ -0,0 +1,9 @@
+module Main (main) where
+
+-- import Lib
+import Shortener
+
+
+main :: IO ()
+--main = someFunc
+main = shortener
---
+++ src/Shortener.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Shortener (shortener, printTime, printConfig, printNumbers, printJson) where
+
+import Web.Scotty
+import Data.Time (getCurrentTime)
+import Data.Aeson (encode)
+
+
+shortener :: IO ()
+shortener =
+    scotty 3000 $
+        get "/" $
+        html "<h1>Welcome</h1>"
+
+printNumbers :: IO ()
+printNumbers = do
+  print (3+4)
+
+printConfig :: IO ()
+printConfig = do
+  contents <- readFile "stack.yaml"
+  putStrLn contents
+
+
+printTime :: IO ()
+printTime = do
+  time <- getCurrentTime
+  print time
+
+numbers :: [Integer]
+numbers = [1,2,3,4]
+
+printJson :: IO ()
+printJson = print (encode numbers)
+-- funcion :: [Char] -> Bool
+-- funcion palabra = True 
